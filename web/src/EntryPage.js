b58146a1034d75639a7f7accff79d763ef8d3f92
SuccessZ <zhaoyaopeng@seu.edu.cn>
2024-10-14 16:20:58 +0800

    fix: error message

--- web/src/EntryPage.js
+++ web/src/EntryPage.js
@@ -95,12 +95,12 @@ class EntryPage extends React.Component {
         });
     };
 
-    if (this.state.application?.isRestricted) {
-      return Util.renderMessageLarge(this, `${i18next.t("check:Your IP address has been banned according to the configuration of application")} ${this.state.application.name}`);
+    if (this.state.application?.ipRestriction) {
+      return Util.renderMessageLarge(this, this.state.application.ipRestriction);
     }
 
-    if (this.state.application?.organizationObj?.isRestricted) {
-      return Util.renderMessageLarge(this, `${i18next.t("check:Your IP address has been banned according to the configuration of organization")} ${this.state.application.organizationObj.name}`);
+    if (this.state.application?.organizationObj?.ipRestriction) {
+      return Util.renderMessageLarge(this, this.state.application.organizationObj.ipRestriction);
     }
 
     const isDarkMode = this.props.themeAlgorithm.includes("dark");
